ddca43feb4ec29eff10ede19360b9916b87856de
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-08 15:22:20 -0700

    MVP fully done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import axios from 'axios';
 import Header from './components/Header';
 import Picture from './components/Picture';
+import apiUrl from './constants';
 
 function App() {
   const [imgSrc, SetImgSrc] = useState("");
   useEffect(()=>{
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=fSXeZrZdkhRWcNOlLhGsx01jlU8b5VQmZSB4w7F0`)
+    axios.get(apiUrl)
       .then(resp => SetImgSrc(resp.data.url))
       .catch(err => console.error(err));
   }, []);
